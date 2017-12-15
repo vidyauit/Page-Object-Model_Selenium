bd41d4f7731312c8610c02fd3f579cac040b825e
vidyauit <vidyacs0104@gmail.com>
2017-12-15 14:52:27 +0530

    triangle

---
+++ Sorting/src/mypkg/Star.java
@@ -0,0 +1,20 @@
+package mypkg;
+
+public class Star {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int i, j, n=1;
+	        for(i=0; i<5; i++)
+	        {
+	            for(j=0; j<=i; j++)
+	            {
+	                System.out.print(n+ " ");
+	                n++;
+	            }
+	            System.out.println();
+	        }
+
+	}
+
+}
